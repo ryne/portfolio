85e6e5ead39e354d6c29dcbca05071c11730fa52
Ryne <ryne@itsrynenotryan.com>
2022-08-30 10:29:06 -0700

    Update button states, add portfolio icon

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,13 @@
 import { useRef, useEffect } from 'react';
 import { gsap } from 'gsap';
 import light from '../assets/light.png';
+import computer from '../assets/omori_computer.gif';
 
 function Hero({ toggleTheme }) {
   const refLight = useRef();
   const refShadow = useRef();
   const refText = useRef();
+  const refComputer = useRef();
 
   useEffect(() => {
     gsap.to(refLight.current, {
@@ -23,41 +25,56 @@ function Hero({ toggleTheme }) {
       ease: 'power4',
     });
     gsap.to(refText.current, {
-      delay: 0.75,
+      delay: 0.7,
       duration: 2,
       ease: 'power1',
       opacity: 1,
     });
+    gsap.to(refComputer.current, {
+      y: -120,
+      delay: 2,
+      duration: 1,
+      ease: 'power4',
+    });
   });
 
   return (
-    <div id="Hero">
-      <div className="flex h-screen justify-center items-center bg-white text-black flex-col selection:bg-black selection:text-white dark:text-white dark:bg-black dark:selection:bg-white dark:selection:text-black transition-opacity transition-colors">
-        <div className="absolute top-50 mb-8 md:mb-16 z-10" ref={refLight}>
-          <div className="absolute -translate-y-full translate-x-4 opacity-10 bg-black w-[1px] h-screen dark:bg-white"></div>
-          <img
-            src={light}
-            alt="light switch"
-            className="cursor-pointer hover(:not focus):opacity-70 active:scale-95 focus:opacity-100 ease-in transition-opacity selection:bg-transparent dark:selection:bg-transparent dark:invert"
-            onClick={() => toggleTheme()}
-          />
-        </div>
-        <div className="opacity-0 text-center" ref={refText}>
-          <h1 className="-mt-1 text-4xl md:text-5xl font-light italic">
-            Ryne
-            <span className="px-2 md:px-3 opacity-10 dark:opacity-20">/</span>
-            Not Ryan
-          </h1>
-          <p className="mt-2 md:mt-3 text-sm md:text-base tracking-[.3em] md:tracking-[.4em] uppercase opacity-50">
-            Web Development &amp; Design
-          </p>
-        </div>
-        <div
-          className="absolute h-0.5 w-10 top-50 mt-20 bg-black blur scale-y-50 dark:bg-white"
-          ref={refShadow}
-        ></div>
+    <section className="flex h-screen justify-center items-center bg-white text-black flex-col selection:bg-black selection:text-white dark:text-white dark:bg-black dark:selection:bg-white dark:selection:text-black transition-opacity transition-colors overflow-hidden">
+      <div className="absolute top-50 mb-8 md:mb-16 z-10" ref={refLight}>
+        <div className="absolute -translate-y-full translate-x-4 opacity-10 bg-black w-[1px] h-screen dark:bg-white"></div>
+        <img
+          src={light}
+          alt="Theme Toggle"
+          className="cursor-pointer active:opacity-50 active:scale-95 ease-in transition-opacity duration-75 selection:bg-transparent dark:selection:bg-transparent dark:invert"
+          onClick={() => toggleTheme()}
+        />
       </div>
-    </div>
+      <div className="opacity-0 text-center" ref={refText}>
+        <h1 className="-mt-1 text-4xl md:text-5xl font-light italic">
+          Ryne
+          <span className="px-2 md:px-3 opacity-10 dark:opacity-20">/</span>
+          Not Ryan
+        </h1>
+        <p className="mt-2 md:mt-3 text-sm md:text-base tracking-[.3em] md:tracking-[.4em] uppercase opacity-50">
+          Web Development &amp; Design
+        </p>
+      </div>
+      <div
+        className="absolute h-0.5 w-10 top-50 mt-20 bg-black blur scale-y-50 dark:bg-white"
+        ref={refShadow}
+      ></div>
+      <div
+        className="absolute w-10 -bottom-12 md:-bottom-16 z-10"
+        ref={refComputer}
+      >
+        <img
+          src={computer}
+          alt="Portfolio Start"
+          className="rendering-pixelated cursor-pointer active:opacity-50 active:scale-95 ease-in transition-opacity duration-75 selection:bg-transparent dark:selection:bg-transparent dark:invert"
+          onClick={() => toggleTheme()}
+        />
+      </div>
+    </section>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,8 @@ module.exports = {
     extend: {},
   },
   variants: {
+    imageRendering: ['responsive'],
     extend: {},
   },
-  plugins: [],
+  plugins: [require('tailwindcss-image-rendering')()],
 };
