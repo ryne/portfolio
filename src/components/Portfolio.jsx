9e175fa56516941c09dfb8cca86109b1cd1a5cfc
Ryne <ryne@itsrynenotryan.com>
2022-09-05 21:18:14 -0700

    Portfolio props and ref

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,9 +1,11 @@
 import { useRef, useEffect } from 'react';
 import { gsap } from 'gsap';
 
-function Portfolio({ showPortfolio }) {
+function Portfolio({ toggleTheme, showPortfolio }) {
+  const refPortfolio = useRef();
+
   return (
-    <section className="flex h-screen justify-center items-center bg-white text-black flex-col selection:bg-black selection:text-white dark:text-white dark:bg-black dark:selection:bg-white dark:selection:text-black transition-opacity transition-colors"></section>
+    <section className="z-10 absolute flex w-screen h-0 justify-center items-center bg-white text-black flex-col selection:bg-black selection:text-white dark:text-white dark:bg-black dark:selection:bg-white dark:selection:text-black transition-opacity transition-colors"></section>
   );
 }
 
